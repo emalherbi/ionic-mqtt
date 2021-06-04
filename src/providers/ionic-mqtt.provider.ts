10b6b5add0284e97ffc82107f3d82204d5a2e165
Eduardo Malherbi <emalherbi@gmail.com>
2021-06-04 13:50:12 -0300

    v1.0.1

--- src/providers/ionic-mqtt.provider.ts
+++ src/providers/ionic-mqtt.provider.ts
@@ -13,7 +13,8 @@ export class IonicMqttService {
         name: 'mqtt',
         // src: 'https://unpkg.com/mqtt/dist/mqtt.min.js',
         // src: '../lib/mqtt/mqtt.js',
-        src: 'https://unpkg.com/mqtt@4.0.0/dist/mqtt.min.js',
+        // src: 'https://unpkg.com/mqtt@4.0.0/dist/mqtt.min.js',
+        src: 'https://unpkg.com/mqtt@4.1.0/dist/mqtt.js',
       },
     ].forEach((script: any) => {
       this.scripts[script.name] = {
